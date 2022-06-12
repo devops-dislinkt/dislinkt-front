f7c25fe9f6ca5acf2ce5ceeb67a47a9350151ea4
Alek <alek@spectra-alarm.com>
2022-06-12 13:42:34 +0200

    fix: home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class HomeComponent implements OnInit {
 
   constructor(
     public userService: UserService,
+    public route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
