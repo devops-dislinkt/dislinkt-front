835655f148ba3d6cd1c1590956a52b9c1dd4533f
Alek <alek@spectra-alarm.com>
2022-06-12 12:22:40 +0200

    feat: modeled profile

--- src/app/model/profile.model.ts
+++ src/app/model/profile.model.ts
@@ -1,9 +1,37 @@
 export class Profile {
-    id: number;
-    username: string;
-    email:  string;
-    firstName:  string;
-    lastName:  string;
-    role: 'admin' | 'user';
-    private: boolean;
+  username: string
+  id: number
+
+  // basic info
+  first_name: string
+  last_name: string
+  email: string
+  role: 'admin' | 'user'
+  phone_number: string
+  birthday: Date
+  biography: string
+  skills: string
+  interests: string
+  private: boolean
+
+  // work experience
+  work_experience: {
+    title: string
+    type: 'FULL_TIME' | 'PART_TIME' | 'INTERNSHIP' | 'FREELANCE' | 'CONTRACT'
+    company: string
+    location: string
+    currently_working: string
+    start_date: Date
+    end_date: Date
+  }
+
+  // education
+  education: {
+    school: string
+    degree: string
+    field_of_study: string
+    start_date: Date
+    end_date: Date
+    description: string
+  }
 }
