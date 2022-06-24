e36088bed40a5123fe4d14337bf888487a9c86c9
Alek <alek@spectra-alarm.com>
2022-06-24 17:17:57 +0200

    fix: can contact if follow and not blocked

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Profile } from 'src/app/model/profile.model';
+import { FollowService } from 'src/app/services/follow.service';
 import { MessagesService } from 'src/app/services/messages.service';
 import { ProfileService } from 'src/app/services/profile.service';
 import { UserService } from 'src/app/services/user.service';
@@ -17,20 +18,21 @@ export class MessagesComponent implements OnInit {
   chatWith = ''
 
   myUsername: string
-  messages: {'from': string, 'value': string, 'to_whom'?: string}[] = []
+  messages: { 'from': string, 'value': string, 'to_whom'?: string }[] = []
   newMsg = ''
   friends: string[] = []
-  
+
   constructor(
     private messagesService: MessagesService,
     private authService: UserService,
     private profileService: ProfileService,
     public fb: FormBuilder,
+    private followService: FollowService,
   ) {
     this.searchForm = this.fb.group({
       'search': ['', [Validators.minLength(3), Validators.required]]
     })
-   }
+  }
 
   ngOnInit(): void {
     this.messagesService.msgReceived.subscribe(msg => this.receiveMessage(msg))
@@ -56,19 +58,36 @@ export class MessagesComponent implements OnInit {
     this.newMsg = '' // clear content
   }
 
-  receiveMessage(msg: {'from': string, 'value': string}) {
+  receiveMessage(msg: { 'from': string, 'value': string }) {
     this.addFriend(msg.from)
     this.messages.unshift(msg)
   }
 
   async search() {
-    if(!this.searchForm.valid) return
+    if (!this.searchForm.valid) return
     const searchString = this.searchForm.value.search
-    const foundProfiles = await this.profileService.searchProfiles(searchString)
-    this.profiles = foundProfiles
+    let foundProfiles = await this.profileService.searchProfiles(searchString)
 
-    // avoid myself
-    this.profiles = this.profiles.filter(p => p.username !== this.myUsername)
+    // // remove myself
+    foundProfiles = foundProfiles.filter(p => p.username !== this.myUsername)
+
+
+    this.profiles = await this.filter(foundProfiles, async (p: Profile) => {
+      const isBlockedByMe = await this.profileService.isProfileBlockedByMe(p)
+      const followRequestStatus = await this.followService.getFollowingRequestStatus(p)
+      if (followRequestStatus != 'APPROVED' || isBlockedByMe) {
+      }
+      else {
+        return p
+      }
+    })
+
+  }
+
+  // helper function for async filter
+  async filter(arr: any[], callback: any) {
+    const fail = Symbol()
+    return (await Promise.all(arr.map(async item => (await callback(item)) ? item : fail))).filter(i=>i!==fail)
   }
 
 }
